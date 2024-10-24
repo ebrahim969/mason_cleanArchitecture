600d5c45df08302d79ec904b62a1498bdd870fba
ebrahim969 <ebrahimelnemr084@gmail.com>
2024-10-24 12:46:20 +0300

    repo

--- bricks/feature/__brick__/{{featureName}}/data/repository_impl/{{featureName}}_repository_impl.dart
+++ bricks/feature/__brick__/{{featureName}}/data/repository_impl/{{featureName}}_repository_impl.dart
@@ -1,5 +1,5 @@
 import '../data_source/remote_data_source/{{featureName}}_remote_data_source.dart';
-import '../repository/{{featureName}}_repository.dart';
+import '../../domain/repository/{{featureName}}_repository.dart';
 
 class {{featureName.pascalCase()}}RepositoryImpl implements {{featureName.pascalCase()}}Repository {
   final {{featureName.pascalCase()}}RemoteDataSource remoteDataSource;
